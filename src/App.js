a40ac6626df1b16962424b93ed64f9ac67a65ad3
LeeJungHoon <gns14585@naver.com>
2023-11-02 17:32:52 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 function App() {
   return (
     <div>
-
+      <h1>hello react</h1>
+      <h2>Hi React</h2>
+      <MyComp />
     </div>
   );
+}
+
+
+
+function MyComp() {
+  return (
+    <div>
+      <h1>HI COMPONENT</h1>
+    </div>
+  )
 }
 
 export default App;
