d5df6ca5d1216552ea8ac3e4c964d7aeaa3fc8ab
LeeJungHoon <gns14585@naver.com>
2023-11-02 17:37:43 +0900

    component 연습, 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+function MyComponent1() {
+  return (
+    <div>
+      <h1>Hello Component</h1>
+    </div>
+  );
+}
+
+function MyComponent2() {
+  return (
+    <div>
+      <p>Lorem ipsum dolor.</p>
+      <ul>
+        <li>Lorem.</li>
+        <li>Lorem.</li>
+        <li>Lorem.</li>
+      </ul>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      <MyComponent2 />
     </div>
   );
 }
