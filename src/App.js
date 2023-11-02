fc820882ef94a79123187ad83e7408db0bdd3fde
LeeJungHoon <gns14585@naver.com>
2023-11-02 17:47:09 +0900

    종료태그 작성 연습 완료, 컴포넌트 여러번 사용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+function MyComponent1() {
+  return (
+    <div style={{ width: "100%", height: "40%" }}>
+      <img
+        style={{ width: "50%" }}
+        src="https://i.imgur.com/SuL4x4v.jpeg"
+        alt="경복궁"
+      />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      <MyComponent1 />
+      <MyComponent1 />
     </div>
   );
 }
