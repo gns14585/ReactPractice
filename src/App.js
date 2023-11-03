f77e332c5f708fc7675c92d0aec22e5e99d5bd4a
이정훈 <gns14585@naver.com>
2023-11-03 22:02:43 +0900

    props destructuring assignment 구조분해할당 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 function App() {
   return (
     <div>
+      <MyBox
+        p="10px"
+        m="10px"
+        color="blue"
+        bg="gold"
+        text="안녕하세요 이정훈입니다."
+      />
+    </div>
+  );
+}
 
+function MyBox({ p, m, color, text, bg }) {
+  return (
+    <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
+      <h1>{text}</h1>
     </div>
   );
 }
