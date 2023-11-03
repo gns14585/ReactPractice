a397eac541a6d1fabafe35b5e05da956ddb511de
LeeJungHoon <gns14585@naver.com>
2023-11-03 17:17:37 +0900

    jsx에서 js코드 작성은 {} 사용

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ function App() {
       <h1>hello react</h1>
       <h2>Hi React</h2>
       <MyComp />
+      <MyComp2 />
     </div>
   );
 }
-
 function MyComp() {
   return (
     <div>
       <h1>HI COMPONENT</h1>
+    </div>
+  );
+}
+function MyComp2() {
+  let name = "손흥민";
+  let age = 33;
+  return (
+    <div>
+      <h1>hello react</h1>
+      <h1>hello {name}</h1>
+      <h1>{age} years</h1>
+      <h1>{age * 2} double years</h1>
+      <h1>{name + "선수"}</h1>
     </div>
   );
 }
