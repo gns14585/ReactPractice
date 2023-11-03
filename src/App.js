d442854c7a61d8962d1758032f6adafd4fe51076
이정훈 <gns14585@naver.com>
2023-11-03 23:08:24 +0900

    component를 다른 파일에 작성 후 import, export 하기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React from "react";
+// export된 컴포넌트(변수,함수,객체)를 import 해서 사용
+import { MyElem } from "./component/MyElem";
+import { MyComp } from "./component/MyComp";
+import { MyHeader } from "./component/MyHeader";
 
 function App() {
   return (
     <div>
-      <MyComp name="흥민" address="신촌" age={33} city="서울" />
-      <MyComp address="신촌" age={33} city="서울" />
-      <MyComp address="런던" city="영국" />
-      <MyComp name="희찬" age={66} city="파리" address="프랑스" />
-    </div>
-  );
-}
-
-function MyComp({ name = "강인", address, city, age = 100 }) {
-  return (
-    <div>
-      <p>
-        {name} 은 {age}세 이고 {address}, {city}에 산다
-      </p>
+      <MyHeader />
+      <MyComp />
+      <MyElem />
     </div>
   );
 }
---
+++ src/component/MyElem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function MyElem() {
+  return (
+    <div>
+      <p>컴포넌트</p>
+    </div>
+  );
+}
