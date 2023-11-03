d2108f57e02dd7357172133d13a8fc6435b07319
이정훈 <gns14585@naver.com>
2023-11-03 21:38:06 +0900

    props작성, 활용연습 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 function App() {
   return (
     <div>
+      <MyHeader color="white" bg="black" text="내가 만든 제목" />
+      <MyHeader color="orange" bg="skyblue" text="다시 만든 제목" />
+    </div>
+  );
+}
 
+function MyHeader(props) {
+  return (
+    <div style={{ color: props.color, backgroundColor: props.bg }}>
+      <h1>{props.text}</h1>
     </div>
   );
 }
