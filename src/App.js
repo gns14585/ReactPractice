8eec68ebfef05fd7ab768e42289db109a0453b5a
이정훈 <gns14585@naver.com>
2023-11-03 21:45:25 +0900

    props 작성, 활용연습 시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 function App() {
   return (
     <div>
+      <MyText
+        style={{ color: "blue", border: "5px solid gold" }}
+        value="hello"
+      />
+      <MyText
+        style={{ color: "red", border: "5px dotted blue" }}
+        value="안녕하세요."
+      />
+    </div>
+  );
+}
 
+function MyText(props) {
+  return (
+    <div style={props.style}>
+      <p>{props.value}</p>
     </div>
   );
 }
