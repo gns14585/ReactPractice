84be074ff859baef4f060e3a53d66f1c4fa78f6a
LeeJungHoon <gns14585@naver.com>
2023-11-03 17:11:54 +0900

    임시커밋

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ function App() {
   return (
     <div>
       <h1>hello react</h1>
-      <h2>Hi React</h2>
+      <h2>hi React</h2>
       <MyComp />
     </div>
   );
 }
-
-
 
 function MyComp() {
   return (
     <div>
       <h1>HI COMPONENT</h1>
     </div>
-  )
+  );
 }
 
 export default App;
