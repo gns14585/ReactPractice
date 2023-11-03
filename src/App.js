a67edc39c1499653df2a9008d4587f3013f6a7fc
이정훈 <gns14585@naver.com>
2023-11-03 21:58:38 +0900

    props destructuring assignment 구조분해할당 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+function MyElem({ name, age, team }) {
+  return (
+    <div>
+      <ul>
+        <li>이름 : {name}</li>
+        <li>나이 : {age}</li>
+        <li>소속 : {team}</li>
+      </ul>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-
+      <MyElem name="흥민" age={44} team="토트넘" />
+      <MyElem name="강인" age={33} team="파리" />
     </div>
   );
 }
