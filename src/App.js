0900a69e660f54b71def75070f5bd70966486833
이정훈 <gns14585@naver.com>
2023-11-04 21:36:43 +0900

    필요없는 파일들 삭제

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-// style 적용 방법
-// 보통 css 파일
-// css modul 사용
-// chakraui css library
-
-import { MyComp } from "./component/MyComp";
-import { MyBox } from "./component/MyBox";
-import { MyContainer } from "./component/MyContainer";
-
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-      <MyBox />
-      <MyContainer />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
