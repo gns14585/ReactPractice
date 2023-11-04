e19fd62ad6b35aed1d79c0955d997a6941b20013
이정훈 <gns14585@naver.com>
2023-11-04 22:07:31 +0900

    bootstrap navbar 만들어 보기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import { Box } from "@chakra-ui/react";
+import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 function App(props) {
   return (
-    <div>
-      <Box bgColor="tomato">Lorem ipsum dolor.</Box>
-      <Box bgColor="red">Lorem ipsum dolor.</Box>
-      <Box bgColor="blue">Dolore, sint ut!</Box>
-      <Box bgColor="blue.50">Incidunt, iste, voluptatem.</Box>
-      <Box bgColor="blue.100">Animi, dignissimos nobis!</Box>
-      <Box bgColor="blue.900">Ea eius, reiciendis?</Box>
-      <Box bgColor="gray">Consequuntur, quos, ut.</Box>
-      <Box bgColor="gray.100">Incidunt, pariatur, ratione!</Box>
-      <Box bgColor="gray.500">Autem eos, voluptates.</Box>
-      <Box bg="yellow.600" color="gray.200">
-        Possimus sit, unde?
-      </Box>
-      <Box bg="facebook.500" color="linkedin.100">
-        Molestias, nobis veritatis!
-      </Box>
-    </div>
+    <Flex
+      px={{ base: "0px", md: "70px", lg: "120px", xl: "180px" }}
+      justify="space-between"
+      py="8px"
+      my="10px"
+      bg="gray.100"
+    >
+      <Flex gap="7px" align="center">
+        <Text as="b">Navbar</Text>
+        <Button variant="ghost">Home</Button>
+        <Button variant="ghost">Link</Button>
+
+        <Menu>
+          <MenuButton
+            variant="ghost"
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+          >
+            Dropdown
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+
+        <Button variant="ghost" isDisabled>
+          Disabled
+        </Button>
+      </Flex>
+      <Flex gap="7px" align="center">
+        <Input size="sm" bg="white" colorScheme="green" />
+        <Button size="sm" variant="outline" colorScheme="green">
+          Search
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
 
