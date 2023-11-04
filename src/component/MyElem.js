811cf7f1e8a8c8d36f150e409436807c4f8c977a
이정훈 <gns14585@naver.com>
2023-11-04 19:46:40 +0900

    한 파일에서 여러개의 값(객체, 값, 함수) export 가능 연습 완료

---
+++ src/component/MyElem.js
@@ -0,0 +1,15 @@
+export function MyElem() {
+  return null;
+}
+
+export function MyNavBar() {
+  return null;
+}
+
+export function MyContent() {
+  return null;
+}
+
+export const country = "한국";
+export let text = "하위";
+export let value = "안녕하세요";
