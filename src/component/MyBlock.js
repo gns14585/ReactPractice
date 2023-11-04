17f2e69cb60a26adeaf773e3952c1be42dab0b16
이정훈 <gns14585@naver.com>
2023-11-04 21:12:03 +0900

    보통의 css 파일을 사용할 때 클래스명 중복이 발생할 수 있음 -> css module로 중복 방지

---
+++ src/component/MyBlock.js
@@ -0,0 +1,8 @@
+import style1 from "../style/yourStyle.module.css";
+export function MyBlock() {
+  return (
+    <div>
+      <h1 className={style1.error}>Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
--- src/component/MyText.js
+++ src/component/MyText.js
@@ -1,8 +1,8 @@
-import "../style/myCss.css";
+import style2 from "../style/myCss.module.css";
 export function MyText() {
   return (
     <div>
-      <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+      <h1 className={style2.error}>Lorem ipsum dolor sit amet.</h1>
     </div>
   );
 }
