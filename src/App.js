05577931d4bd695ee108f6d575edb905a3aef2f4
이정훈 <gns14585@naver.com>
2023-11-05 12:51:17 +0900

    이벤트 핸들러 메소드에 event 객체가 전달된다

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  // 함수명 작성 관습
-  // handle이벤트명
-  const handleClick = () => {
-    console.log("second");
-  };
-  function handleMouseEnter() {
-    console.log("third");
-  }
-  function handleMouseLeave() {
-    console.log("4th");
+  function handleChange(e) {
+    // console.log("인풋값이 해당됨");
+    console.log(e.target.value);
   }
   return (
     <div>
-      <Button onClick={() => console.log("first")}>버튼1</Button>
-      <Button onClick={handleClick}>버튼2</Button>
-      <Button onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        버튼3
-      </Button>
+      <Input onChange={handleChange} />
+      <Input onChange={handleChange} />
     </div>
   );
 }
