42d085a88270e29d8baf95eb0b56e141324df1af
이정훈 <gns14585@naver.com>
2023-11-05 12:34:08 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
-import { Button } from "@chakra-ui/react";
+import React from "react";
 
-function MyComp({ children, executeClick }) {
-  return <Button onClick={executeClick}>{children}</Button>;
-}
 function App(props) {
-  function func1() {
-    console.log("func1 실행");
-  }
-
-  let func2 = () => {
-    console.log("arrow function 실행1");
-  };
-
-  return (
-    <div>
-      <MyComp executeClick={func1}>Button1</MyComp>
-      <MyComp executeClick={func2}>Button2</MyComp>
-      <MyComp
-        executeClick={() => {
-          console.log("arrow function 실행222");
-        }}
-      >
-        Button3
-      </MyComp>
-      <MyComp executeClick={() => console.log("arrow function 실행 444")}>
-        Button4
-      </MyComp>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
