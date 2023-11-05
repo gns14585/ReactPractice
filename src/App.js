bc8f5f3645e114b47de63416ee750fc2ea9b5688
이정훈 <gns14585@naver.com>
2023-11-05 11:58:10 +0900

    children prop 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import * as PropTypes from "prop-types";
+import { Button } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    <>
+      <Button as="a" href={to} colorScheme="orange">
+        {children}
+      </Button>
+    </>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyLink to="https://www.naver.com">naver 로</MyLink>
+      <MyLink to="https://www.daum.net">다음 으로</MyLink>
+    </div>
+  );
 }
 
 export default App;
