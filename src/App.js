83ae1911a4833af6ef332a3e2bb145c56b9dff66
이정훈 <gns14585@naver.com>
2023-11-05 13:08:46 +0900

    event.preventDefault() : 기본 기능 취소 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function handleClick(e) {
+    e.preventDefault();
+    window.location.href = "https://www.naver.com";
+  }
+  return (
+    <div>
+      <a href="http://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
+    </div>
+  );
 }
 
 export default App;
