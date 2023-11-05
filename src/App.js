28232284f4ecb2a93f4fc18ff8a57b239bb91973
이정훈 <gns14585@naver.com>
2023-11-05 12:18:49 +0900

    함수를 props으로 전달 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Button } from "@chakra-ui/react";
+
+function MyButton({ executeClick, children }) {
+  return (
+    <div>
+      <Button onClick={executeClick}>{children}</Button>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  const func1() {
+    console.log("hello")
+  }
+  return (
+    <div>
+      <MyButton executeClick={func1} >hello</MyButton>
+      <MyButton>greeting</MyButton>
+    </div>
+  );
 }
 
 export default App;
