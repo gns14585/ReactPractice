5d1c3aefa43dd3aaf752e4b7b5b7d9340711c014
이정훈 <gns14585@naver.com>
2023-11-05 19:04:50 +0900

    event.stopPropagation : event bublling 막는 기능 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Button, Center } from "@chakra-ui/react";
 
-function App(props) {
-  return <div></div>;
+function App() {
+  function handleOuterBoxClick(e) {
+    e.stopPropagation();
+    console.log("바깥 상자 클릭");
+  }
+  function handleInnerBoxClick(e) {
+    e.stopPropagation();
+    console.log("안쪽 상자 클릭");
+  }
+  function handleButtonClick(e) {
+    e.stopPropagation();
+    console.log("버튼 클릭됨");
+  }
+
+  return (
+    <>
+      <Center onClick={handleOuterBoxClick} w="200px" h="200px" bg="red">
+        <Center onClick={handleInnerBoxClick} w="100px" h="100px" bg="blue">
+          <Button onClick={handleButtonClick} colorScheme="purple">
+            Button
+          </Button>
+        </Center>
+      </Center>
+    </>
+  );
 }
 
 export default App;
