7491a9bd1775d16bfd9f41fc2781126e7cca82c0
LeeJungHoon <gns14585@naver.com>
2023-11-06 17:24:34 +0900

    state 사용 예제

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@chakra-ui/react";
 
 function App(props) {
+  const [text, setText] = useState("hello");
+  console.log(text);
+
+  function handleButtonClick() {
+    // text 라는 state 값 변경
+    setText("greeting");
+  }
+
   return (
     <div>
-      <h1></h1>
+      <Button onClick={handleButtonClick}>상태 변경</Button>
+      <p>{text}</p>
     </div>
   );
 }
