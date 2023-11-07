240ebd6781c143cd1a192d3065f2d0944ca35d64
이정훈 <gns14585@naver.com>
2023-11-07 22:28:55 +0900

    ajax get 사용 state 업데이트 연습

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App(props) {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("api/main1/sub2")
+      .then((response) => response.data)
+      .then((data) => setMessage(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
