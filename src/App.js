bb5fd5afbba4ebda666fa5ebd3d3095cd062331b
이정훈 <gns14585@naver.com>
2023-11-07 20:15:24 +0900

    임시

--- src/App.js
+++ src/App.js
@@ -1,29 +1,79 @@
 import React, { useState } from "react";
-import { Box, Button, Text } from "@chakra-ui/react";
-
-function MyComp1({ setMessage }) {
-  return (
-    <Box>
-      <Button onClick={() => setMessage("안녕하세요")}>텍스트 바꾸기</Button>
-    </Box>
-  );
-}
-
-function MyComp2({ message }) {
-  return (
-    <Box>
-      <Text>{message}</Text>
-    </Box>
-  );
-}
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 
 function App(props) {
-  const [message, setMessage] = useState("hello");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
+  function handleNameInput(e) {
+    setName(e.target.value);
+  }
+
+  function handlePasswordInput(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleEmailInput(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleSubmit() {
+    setSubmitting(true);
+    /*
+    axios.post("/adduser", {
+      name, email, password
+    }).then(response => console.log("성공할 때 해야하는 일"))
+      .catch(error => console.log("실패할 때 해야하는 일"))
+      .finally(() => console.log("무조건 해야하는 일"))
+  */
+  }
+
   return (
-    <div>
-      <MyComp1 setMessage={setMessage}></MyComp1>
-      <MyComp2 message={message}></MyComp2>
-    </div>
+    <Center>
+      <Box w={"480px"}>
+        <FormControl mb={5}>
+          <FormLabel>Name</FormLabel>
+          <Input type="text" value={name} onChange={handleNameInput} />
+          <FormHelperText>띄어쓰기 없이 입력해주세요.</FormHelperText>
+        </FormControl>
+
+        <FormControl mb={5}>
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            value={password}
+            onChange={handlePasswordInput}
+          />
+          <FormHelperText>
+            특수기호와 숫자를 하나 이상 작성해주세요.
+          </FormHelperText>
+        </FormControl>
+
+        <FormControl mb={5}>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" value={email} onChange={handleEmailInput} />
+          <FormHelperText>입력된 이메일은 중복될 수 없습니다.</FormHelperText>
+        </FormControl>
+
+        <Button
+          onClick={handleSubmit}
+          colorScheme="blue"
+          isLoading={submitting}
+        >
+          가입
+        </Button>
+      </Box>
+    </Center>
   );
 }
 
