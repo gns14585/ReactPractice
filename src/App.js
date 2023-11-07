6556e44a0b69ae3e9480118c183548411730bbb2
이정훈 <gns14585@naver.com>
2023-11-07 21:52:33 +0900

    컴포넌트 밖의 값을 변경하지 않도록

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { createContext, useState } from "react";
-import { AComp } from "./AComp";
-import { BComp } from "./BComp";
+import React from "react";
+
+function MyComp() {
+  return <div>hello</div>;
+}
 
 function App(props) {
-  const [message, setMessage] = useState("초기메세지");
-
   return (
     <div>
-      <MessageContext.Provider value={{ message, setMessage }}>
-        <AComp />
-        <BComp />
-      </MessageContext.Provider>
+      <MyComp />
     </div>
   );
 }
-
-export const MessageContext = createContext(null);
 
 export default App;
--- src/BComp.js
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from "react";
-import { Text } from "@chakra-ui/react";
-import { MessageContext } from "./App";
-
-export function BComp() {
-  const { message } = useContext(MessageContext);
-  return <Text>{message}</Text>;
-}
