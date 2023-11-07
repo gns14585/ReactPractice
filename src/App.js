548c810d52a133af46eaffe932e591d27551bdb9
LeeJungHoon <gns14585@naver.com>
2023-11-07 17:37:14 +0900

    useImmer 로 객체상태 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Input, Text } from "@chakra-ui/react";
+import { useImmer } from "use-immer";
 
 function App(props) {
-  return <div></div>;
+  const [person1, setPerson1] = useState({
+    name: "son",
+    city: "seoul",
+    email: "son@gamil.com",
+  });
+
+  const [person2, updatePerson2] = useImmer({
+    name: "kim",
+    city: "jeju",
+    email: "kim@naver.com",
+  });
+
+  function handleNameChange(e) {
+    const copyPerson1 = { ...person1 };
+    copyPerson1.name = e.target.value;
+    setPerson1(copyPerson1);
+  }
+
+  function handleCityChange(e) {
+    setPerson1({ ...person1, city: e.target.value });
+  }
+
+  function handleEmailChange(e) {
+    setPerson1({ ...person1, email: e.target.value });
+  }
+
+  function handleNameChange2(e) {
+    updatePerson2((draft) => {
+      draft.name = e.target.value;
+    });
+  }
+
+  function handleCityChange2(e) {
+    updatePerson2((draft) => {
+      draft.city = e.target.value;
+    });
+  }
+
+  function handleEmailChange2(e) {
+    updatePerson2((draft) => {
+      draft.email = e.target.value;
+    });
+  }
+
+  return (
+    <div>
+      <Box>
+        <Input value={person1.name} onChange={handleNameChange} />
+        <Text>이름 : {person1.name}</Text>
+
+        <Input value={person1.city} onChange={handleCityChange} />
+        <Text>도시 : {person1.city}</Text>
+
+        <Input value={person1.email} onChange={handleEmailChange} />
+        <Text>메일 : {person1.email}</Text>
+      </Box>
+
+      <hr />
+      <Box>
+        <Input value={person2.name} onChange={handleNameChange2} />
+        <Text>이름 : {person2.name}</Text>
+
+        <Input value={person2.city} onChange={handleCityChange2} />
+        <Text>도시 : {person2.city}</Text>
+
+        <Input value={person2.email} onChange={handleEmailChange2} />
+        <Text>메일 : {person2.email}</Text>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
