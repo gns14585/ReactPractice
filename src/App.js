ab950437700e4d034c349af9e0613e253e0fbf72
이정훈 <gns14585@naver.com>
2023-11-08 21:27:49 +0900

    useEffect 예제

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import React, { useEffect, useState } from "react";
-import { Button, Input, Text } from "@chakra-ui/react";
+import { Box, Select, Spinner, Text } from "@chakra-ui/react";
+import axios from "axios";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-  const [text, setText] = useState("");
+  const [customerId, setCustomerId] = useState(0);
+  const [customer, setCustomer] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    // 첫번째 파라미터 : The function with your Effect's logic 부작용(부수적)이 있는 함수
-    // 두번쨰 파라미터 : setup을 실행시키는 값의 나열(배열)
-    //                 빈 배열이면 첫 렌더링 때만 실행
-    console.log("effect 함수 실행됨");
-  }, []);
+    setIsLoading(true);
+    axios
+      .get("/api/main1/sub5?id=" + customerId)
+      .then((response) => response.data)
+      .then((data) => setCustomer(data))
+      .catch((error) => setCustomer(null))
+      .finally(() => setIsLoading(false));
+  }, [customerId]); // 페이지를 첫 로딩할때도 생성되고, 번호를 바꿔도 생성됨
 
   return (
     <div>
-      <Button onClick={() => setNumber(number + 1)}>증가</Button>
-      <Text>{number}</Text>
-      <Input value={text} onChange={(e) => setText(e.target.value)} />
-      <Text>{text}</Text>
+      <Select
+        placeholder="고객 번호를 선택하세요"
+        onChange={(e) => setCustomerId(e.target.value)}
+      >
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </Select>
+      <Box>
+        {isLoading && <Spinner />}
+        {isLoading || (
+          <>
+            {customer === null ? (
+              <Text>조회한 고객이 없습니다.</Text>
+            ) : (
+              <Text>고객 이름 : {customer.name}</Text>
+            )}
+          </>
+        )}
+      </Box>
     </div>
   );
 }
